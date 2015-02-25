235de7e9a83843a77f4e17cb53cbbe41736268c1
xedoc <anton.meshkov@gmail.com>
2015-02-25 15:39:44 +0200

    UI scetch

---
+++ IL2CDR/IL2CDR/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using IL2CDR.ViewModel;
+using MahApps.Metro.Controls;
+
+namespace IL2CDR
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+    }
+}
---
+++ IL2CDR/IL2CDR/Model/DataService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace IL2CDR.Model
+{
+    public class DataService : IDataService
+    {
+        public void GetData(Action<DataItem, Exception> callback)
+        {
+            // Use this to connect to the actual data service
+            var item = new DataItem("Welcome to MVVM Light");
+            callback(item, null);
+        }
+    }
+}
