c39d780721239b636d86fbc6db7dcaa663c50dc0
xedoc <anton.meshkov@gmail.com>
2015-02-26 16:00:05 +0200

    ScriptManager

---
+++ IL2CDR/IL2CDR/Model/ScriptManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSScriptLibrary;
+
+namespace IL2CDR.Model
+{
+    public class ScriptManager
+    {
+        private const string scriptsSubFolder = @"\Scripts";
+        private static Random random = new Random();
+
+        public void LoadScripts()
+        {
+            string folder = AppDomain.CurrentDomain.GetData("DataDirectory") + scriptsSubFolder;
+
+            var scriptFiles = Directory.GetFiles(folder, "*.cs", SearchOption.AllDirectories);
+            foreach (var scriptFileName in scriptFiles)
+            {
+                Util.Try(() => {
+                    Log.WriteInfo("Loading script {0}...", scriptFileName);
+                    var scriptObject = CSScript.Evaluator.LoadFile(scriptFileName);
+
+                    //Is known script ?
+                    if (scriptObject is IActionScript)
+                    {
+                        var script = scriptObject as IActionScript;
+                    }
+                
+                });
+            }
+        }
+    }
+}
