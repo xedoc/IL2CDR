a3f11762432b5eadc7ceb7273b050d965dc2ffd3
xedoc <anton.meshkov@gmail.com>
2015-03-09 09:59:43 +0200

    Rcon commands

--- IL2CDR/IL2CDR/Model/RconConnection.cs
+++ IL2CDR/IL2CDR/Model/RconConnection.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -12,15 +13,57 @@ namespace IL2CDR.Model
 {
     public class RconConnection : NotifyPropertyChangeBase, IStopStart
     {
+        private object lockConnection = new object();
         private TcpClient connection;
         private NetworkStream netStream;
         private bool isStopped = false;
+        private Dictionary<string, string> errorCodes = new Dictionary<string, string>()
+        {
+            {"1", "OK"},
+            {"2", "Unknown result"},
+            {"3", "Unknown command"},
+            {"4", "Incorrect parameters count"},
+            {"5", "Receive buffer error"},
+            {"6", "Access denied! Authenticate first!"},
+            {"7", "Server is not running" },
+            {"8", "This user isn't allowed to execute a command"},
+            {"9", "Invalid user"}
+        };
         
         public RconConnection(IL2StartupConfig config)
         {
             Config = config;
         }
 
+        /// <summary>
+        /// The <see cref="LastErrorDescription" /> property's name.
+        /// </summary>
+        public const string LastErrorDescriptionPropertyName = "LastErrorDescription";
+
+        private string _lastErrorDescription;
+
+        /// <summary>
+        /// Sets and gets the LastErrorDescription property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public string LastErrorDescription
+        {
+            get
+            {
+                return _lastErrorDescription;
+            }
+
+            set
+            {
+                if (_lastErrorDescription == value)
+                {
+                    return;
+                }
+
+                _lastErrorDescription = value;
+                RaisePropertyChanged(LastErrorDescriptionPropertyName);
+            }
+        }
 
         /// <summary>
         /// The <see cref="Config" /> property's name.
@@ -136,70 +179,108 @@ namespace IL2CDR.Model
                 Util.Try(() => connection.Close());
             }
         }
-        private void Authorize()
+        private NameValueCollection RawCommand(string line)
         {
-            var result = WriteLine(String.Format("auth {0} {1}", Config.Login, Config.Password));
-            //Log.WriteInfo(result["STATUS"]);
-        }
-
-        private NameValueCollection WriteLine(string line)
-        {
-            if (isStopped)
-                return new NameValueCollection();
-
-            if(netStream == null || !netStream.CanWrite)
-                Start();
-
-            if (netStream.CanWrite)
-            {                
-                Byte[] sendBytes = Encoding.UTF8.GetBytes(String.Concat(line));
-                Byte[] length = BitConverter.GetBytes((ushort)(line.Length+1));
-                Byte[] zero = { 0 };
-                Byte[] packet = length.Concat(sendBytes).Concat(zero).ToArray();                                
-                Util.Try(() => netStream.Write(packet, 0, packet.Length),false);
-            }
-            else
+            lock( lockConnection )
             {
-                Disconnect();
-                return null;
-            }
+                LastErrorDescription = "Communication error";
 
-            if (netStream.CanRead )
-            {
-                if( netStream.DataAvailable )
+                if (isStopped)
+                    return new NameValueCollection();
+
+                if (netStream == null || !netStream.CanWrite)
+                    Start();
+
+                if (netStream.CanWrite)
                 {
-                    byte[] bytes = new byte[connection.ReceiveBufferSize];
-                    Util.Try(() => netStream.Read(bytes, 0, (int)connection.ReceiveBufferSize), false);
-                    UInt16 length = BitConverter.ToUInt16(bytes.Take(2).ToArray(), 0);
-                    string response = null;
-                    if (length > 2)
-                        response = Encoding.UTF8.GetString(bytes.Skip(2).Take((int)length - 1).ToArray());
-
-                    if (!String.IsNullOrWhiteSpace(response))
-                        return HttpUtility.ParseQueryString(response);
-                    else
-                        return new NameValueCollection();
-
+                    Byte[] sendBytes = Encoding.UTF8.GetBytes(String.Concat(line));
+                    Byte[] length = BitConverter.GetBytes((ushort)(line.Length + 1));
+                    Byte[] zero = { 0 };
+                    Byte[] packet = length.Concat(sendBytes).Concat(zero).ToArray();
+                    Util.Try(() => netStream.Write(packet, 0, packet.Length), false);
                 }
+                else
+                {
+                    Disconnect();
+                    return null;
+                }
+                
+                var writeTask = Task.Factory.StartNew<NameValueCollection>(() =>
+                {
+                    if (netStream.CanRead)
+                    {
+                        while (!netStream.DataAvailable)
+                            Thread.Sleep(1);
+
+                        byte[] bytes = new byte[connection.ReceiveBufferSize];
+                        Util.Try(() => netStream.Read(bytes, 0, (int)connection.ReceiveBufferSize), false);
+                        UInt16 length = BitConverter.ToUInt16(bytes.Take(2).ToArray(), 0);
+                        string response = null;
+                        if (length > 2)
+                            response = Encoding.UTF8.GetString(bytes.Skip(2).Take((int)length - 1).ToArray());
+
+                        if (!String.IsNullOrWhiteSpace(response))
+                        {
+                            var result = HttpUtility.ParseQueryString(response);
+                            string errorText;
+                            LastErrorDescription = String.Empty;
+                            
+                            if( errorCodes.TryGetValue(result["STATUS"], out errorText) )
+                                LastErrorDescription = errorText;
+
+                            return result;
+                        }
+                        else
+                        {
+                            return new NameValueCollection();
+                        }
+                    }
+                    else
+                    {
+                        Disconnect();
+                        Start();
+                    }
+                    return new NameValueCollection();
+                });
+                writeTask.Wait(1000);
+                return writeTask.Result;
             }
-            else
-            {
-                Disconnect();
-                Start();
-            }
-            return new NameValueCollection();
+        }
+        /// <summary>
+        /// Authorizes user on rcon server with user/password taken from the startup.cfg
+        /// </summary>
+        private void Authenticate()
+        {
+            var result = RawCommand(String.Format("auth {0} {1}", Config.Login, Config.Password));
+            Log.WriteInfo(result["STATUS"]);
         }
 
+        /// <summary>
+        /// Get server console text
+        /// </summary>
+        /// <returns>Console text</returns>
         public string GetConsole()
         {
-            return WriteLine("getconsole")["console"];
+            return RawCommand("getconsole")["console"];
+        }
+
+        /// <summary>
+        /// Check if rcon user is authenticated
+        /// </summary>
+        /// <returns>true if authenticated, false if not</returns>
+        public bool AuthenticationStatus()
+        {
+            if( RawCommand("mystatus")["authed"] == "1" )
+                return true;
+
+            return false;
         }
 
         public void Start()
         {
             isStopped = false;
             Connect();
-            Authorize();
+            Authenticate();
         }
 
         public void Stop()
