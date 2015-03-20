9ce6307e2ee03a9f2899674d26b23066ee34aecb
xedoc <anton.meshkov@gmail.com>
2015-03-20 18:56:38 +0200

    Hits class

---
+++ IL2CDR/IL2CDR/Model/Hits.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL2CDR.Model
+{
+    public class Hits
+    {
+        public Hits(string ammo)
+        {
+            Ammo = ammo;
+        }
+        public string AmmoId { get; set; }
+
+        private string ammo;
+        public string Ammo
+        {
+            get { return ammo; }
+            set {                 
+                ammo = value;
+                AmmoId = GuidUtility.Create(GuidUtility.IsoOidNamespace, ammo).ToString(); 
+            }
+        }        
+        public int HitCount { get; set; }
+        public double Damage { get; set; }
+    }
+
+    public class HitsSource
+    {
+        public Player Player { get; set; }
+        public GameObject Object { get; set; }
+        public Hits Hits { get; set; }
+    }
+}
