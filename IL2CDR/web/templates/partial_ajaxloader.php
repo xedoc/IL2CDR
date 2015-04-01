e76a6a78119f7055e45395bbe07f3971797fca0d
xedoc <anton.meshkov@gmail.com>
2015-04-01 11:37:05 +0300

    loader

--- IL2CDR/web/templates/partial_ajaxloader.php
+++ IL2CDR/web/templates/partial_ajaxloader.php
@@ -1,4 +1,4 @@
 
 <div class="col-md-12 top-buffer" id="processingIndicator">
-    <img class="center-block" width="32" height="32" src="images/ajax_loader.gif" />
+    <img class="center-block" width="32" height="32" src="/images/ajax_loader.gif" />
 </div>
